fe2dfb57bdffc0ab41e153e1fb10eff92507c181
nongparanai <promlerd.p@gmail.com>
2023-11-23 15:12:38 +0700

    feat(exercise): add completed exercises

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,13 @@
-//Exercise #1: Variable Creation
-//Start coding here
+let firstName = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
 
-//Exercise #2: Variable Reassignment
-//Start coding here
+const restaurantName = "Chef's Table by Chef James";
+const grandTotal = 15000;
+const openRestaurant = 1500;
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+console.log(address);
+grandTotal = 5000;
+openRestaurant = 250;
+console.log(grandTotal);
