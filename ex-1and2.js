79b9de027fcf254c56f9815a7de63d06232625f3
Mos amon <amonchanchai@gmail.com>
2023-11-23 14:01:22 +0700

    fix:ex1-4

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,12 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let name = "Karter"
+let lastName = "Anderson"
+let address ="54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+const restaurant = "Chef's Table by Chef James"
+const totalPrice = 15000
+const availableRestaurant = 1500
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+totalPrice = 5000
+availableRestaurant = 2500
